0cd70e1f210b5ef535f8a3a0e7896502c8e3f2ac
li <li@LAPTOP-6RG8IG91>
2018-11-05 10:04:21 +0800

    SSM框架搭建

---
+++ 20151104687_liyuhui/20151104687_liyuhui_LaboratoryCloudMonitoringSystem/src/com/lyh/core/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.lyh.core.dao;
+
+import com.lyh.core.po.User;
+
+public class UserDao {
+
+	public User login(String uname, String upwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
