d3d52a3ca3da9524adb606ae45c092b382d0ec24
ciec20151104708 <912987977@qq.com>
2018-11-28 15:38:24 +0800

    修改报错语句

--- 20151104708_maningbo/20151104708_maningbo_NetworkChatSystem/src/cn/chat/pojo/UserBean.java
+++ 20151104708_maningbo/20151104708_maningbo_NetworkChatSystem/src/cn/chat/pojo/UserBean.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package cn.chat.pojo;
 
 public class UserBean {
@@ -25,45 +25,3 @@ public class UserBean {
 	}
 	
 }
-=======
-package cn.chat.pojo;
-
-public class UserBean {
-	private int id;
-	private String userName;
-	private String userPass;
-	private String phoneNumber;
-	private String email;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getUserPass() {
-		return userPass;
-	}
-	public void setUserPass(String userPass) {
-		this.userPass = userPass;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-}
->>>>>>> refs/remotes/origin/master
--- 20151104708_maningbo/20151104708_maningbo_NetworkChatSystem/src/cn/chat/service/UserService.java
+++ 20151104708_maningbo/20151104708_maningbo_NetworkChatSystem/src/cn/chat/service/UserService.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package cn.chat.service;
 
 import java.util.List;
@@ -14,20 +14,4 @@ public interface UserService {
 
 
 }
-=======
-package cn.chat.service;
 
-import java.util.List;
-
-import cn.chat.pojo.UserBean;
-
-public interface UserService {
-	
-	public UserBean findUserByEmail(String email,String userPass);
-	public void addUser(UserBean userBean);
-	public boolean update(String userName,String userPass,String phoneNumber,String email,Integer id);
-	public UserBean findUserById(int id);
-
-
-}
->>>>>>> refs/remotes/origin/master
