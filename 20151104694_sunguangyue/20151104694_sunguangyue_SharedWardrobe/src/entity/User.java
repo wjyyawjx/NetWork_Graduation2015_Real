f9c9a4bd5a1b7fabe43033db8fd09578e1d6e5a7
SGY <1525650347@qq.com>
2018-12-23 00:03:56 +0800

    20151104694-孙光月

---
+++ 20151104694_sunguangyue/20151104694_sunguangyue_SharedWardrobe/src/entity/User.java
@@ -0,0 +1,57 @@
+
+package com.entity;
+
+public class User {
+    private int id;
+    private String name;
+    private String worknumber;
+    private String sex;
+    private String home;
+    private String gangwei;
+    private String info;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getHome() {
+		return home;
+	}
+	public void setHome(String home) {
+		this.home = home;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public String getGangwei() {
+		return gangwei;
+	}
+	public void setGangwei(String gangwei) {
+		this.gangwei = gangwei;
+	}
+	public String getWorknumber() {
+		return worknumber;
+	}
+	public void setWorknumber(String worknumber) {
+		this.worknumber = worknumber;
+	}
+    
+}
+
