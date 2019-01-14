8bc1af8280c0a26f77860fefefabb55f395648b5
SwithunHan <hanshuaidong16@163.com>
2019-01-14 18:34:30 +0800

    对登陆活动进行编写
    初始化界面控件，定义对象及字符串类型变量

--- 20151104701_hanshuaidong/LearningSupervisionSystem/app/src/main/java/com/example/hansh/learningsupervisionsystem/LoginActivity.java
+++ 20151104701_hanshuaidong/LearningSupervisionSystem/app/src/main/java/com/example/hansh/learningsupervisionsystem/LoginActivity.java
@@ -1,16 +1,33 @@
 package com.example.hansh.learningsupervisionsystem;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView tv_back, tv_register, tv_find_psw;
+    private Button btn_login;
+    private String userName, psw, spPsw;
+    private EditText et_user_name, et_psw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
 
+    }
 
+    private void init() {
+        tv_register = findViewById(R.id.tv_register);
+        tv_find_psw = findViewById(R.id.tv_find_psw);
+        btn_login = findViewById(R.id.btn_login);
+        et_user_name = findViewById(R.id.et_user_name);
+        et_psw = findViewById(R.id.et_psw);
     }
 }
