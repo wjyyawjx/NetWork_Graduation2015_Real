280b304b71e35737e2716568558912bd06b816b4
SwithunHan <hanshuaidong16@163.com>
2019-02-01 21:34:13 +0800

    删除主活动中的代码及相关代码，软件重建
    重新设计主页面，不再使用Tab形式的布局

---
+++ 20151104701_hanshuaidong/LearningSupervisionSystem/app/src/main/java/com/example/hansh/learningsupervisionsystem/AddActivity.java
@@ -0,0 +1,15 @@
+package com.example.hansh.learningsupervisionsystem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+    }
+
+}
