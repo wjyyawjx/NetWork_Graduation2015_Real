53d2994839daad5919c28ceadac815ed54d4637d
SwithunHan <hanshuaidong16@163.com>
2019-02-02 23:06:09 +0800

    修改前端欢迎页面及代码
    修改前端的进入应用的按钮，添加了一倒计时自动进入登陆页面的功能

--- 20151104701_hanshuaidong/LearningSupervisionSystem/app/src/main/java/com/example/hansh/learningsupervisionsystem/WelcomeActivity.java
+++ 20151104701_hanshuaidong/LearningSupervisionSystem/app/src/main/java/com/example/hansh/learningsupervisionsystem/WelcomeActivity.java
@@ -1,24 +1,87 @@
 package com.example.hansh.learningsupervisionsystem;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class WelcomeActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private int recLen = 5;
+    private TextView tv;
+    Timer timer = new Timer();
+    private Handler handler;
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        getWindow().setFlags(flag, flag);
         setContentView(R.layout.activity_welcome);
-        Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        initView();
+        timer.schedule(task, 1000, 1000);
+        /**
+         * 正常情况下不点击跳过
+         */
+        handler = new Handler();
+        handler.postDelayed(runnable = new Runnable() {
             @Override
-            public void onClick(View view) {
+            public void run() {
                 Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
-        });
+        }, 5000);
+
+    }
+
+    private void initView() {
+        tv = findViewById(R.id.tv);
+        tv.setOnClickListener(this);
+    }
+
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    recLen--;
+                    tv.setText("跳过 " + recLen);
+                    if (recLen < 0) {
+                        timer.cancel();
+                        tv.setVisibility(View.GONE);
+                    }
+                }
+            });
+        }
+    };
+
+    /**
+     * 点击跳过
+     */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv:
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                if (runnable != null) {
+                    handler.removeCallbacks(runnable);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
+
