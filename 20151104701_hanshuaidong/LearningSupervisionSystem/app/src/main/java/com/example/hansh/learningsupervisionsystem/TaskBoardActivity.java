afba18d4342cd9a05332d32205788a6ed8430b2c
SwithunHan <hanshuaidong16@163.com>
2019-02-13 21:55:06 +0800

    尝试将数据库数据放入listView
    配置Adapter，初始化控件，配置Item

--- 20151104701_hanshuaidong/LearningSupervisionSystem/app/src/main/java/com/example/hansh/learningsupervisionsystem/TaskBoardActivity.java
+++ 20151104701_hanshuaidong/LearningSupervisionSystem/app/src/main/java/com/example/hansh/learningsupervisionsystem/TaskBoardActivity.java
@@ -1,9 +1,17 @@
 package com.example.hansh.learningsupervisionsystem;
 
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.litepal.LitePal;
@@ -11,32 +19,25 @@ import org.litepal.crud.LitePalSupport;
 import org.litepal.exceptions.DataSupportException;
 import org.litepal.tablemanager.Connector;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskBoardActivity extends AppCompatActivity {
     private static final String TAG = "TaskBoardActivity";
-    List<Renwu> RenwuList;
+    private ListView lv;
+    private List<Renwu> renwulist;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taskboard);
-        RenwuList = LitePal.findAll(Renwu.class);
-        for(Renwu renwu:RenwuList){
-            Log.e("TaskBoardActivity","Task conten is" + renwu.getTask_content());
-            Log.e("TaskBoardActivity","Date is" + renwu.getDate());
-            Log.e("TaskBoardActivity","TIme is" + renwu.getTime());
-        }
-
-        /*LinearLayout ll=(LinearLayout) findViewById(R.id.ll);
-        for(Renwu renwu:RenwuList)
-        {
-            //1.集合中每有一条数据，就new一个TextView
-            TextView tv=new TextView(this);
-            //2.把人物的信息设置为文本的内容
-            tv.setText(renwu.toString());
-            tv.setTextSize(18);
-            //3.把TextView设置成线性布局的子节点
-            ll.addView(tv);
-        }*/
+        renwulist = LitePal.findAll(Renwu.class);
+        lv = (ListView) findViewById(R.id.lv);
     }
+
+
+
+
+
+
 }
