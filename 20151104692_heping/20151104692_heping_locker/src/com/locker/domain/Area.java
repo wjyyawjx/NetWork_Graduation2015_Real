c6481dbbfdca3ccb7af4f5fbcf5008087367dab3
hp20151104692 <43986149+hp20151104692@users.noreply.github.com>
2019-02-17 13:54:45 +0800

    地域表
    按地域增删改查储物柜

---
+++ 20151104692_heping/20151104692_heping_locker/src/com/locker/domain/Area.java
@@ -0,0 +1,33 @@
+package com.locker.domain;
+/**
+ * 地区表
+ * @author wuqi
+ *
+ */
+public class Area {
+	/**
+	 * 地区编号
+	 */
+    private String arId;
+
+    /**
+     * 地区名
+     */
+    private String arName;
+
+    public String getArId() {
+        return arId;
+    }
+
+    public void setArId(String arId) {
+        this.arId = arId == null ? null : arId.trim();
+    }
+
+    public String getArName() {
+        return arName;
+    }
+
+    public void setArName(String arName) {
+        this.arName = arName == null ? null : arName.trim();
+    }
+}
