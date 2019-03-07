0ef76e438691dd462ad05125fefcba825dc08ed0
imnu20151104700 <18847137654@163.com>
2019-03-07 16:46:51 +0800

    1

--- 20151104700_guoyunlong/20151104700_guoyunlong_CampusActivityPlatform/src/com/neishida/Servlet/simpleFileupload.java
+++ 20151104700_guoyunlong/20151104700_guoyunlong_CampusActivityPlatform/src/com/neishida/Servlet/simpleFileupload.java
@@ -18,7 +18,7 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;  
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-import com.nba.service.uploadservice;
+import com.neishida.service.uploadservice;
 
 /**
  * Servlet implementation class simpleFileupload
