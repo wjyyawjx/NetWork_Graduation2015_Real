2ce2d740f76a23a340e7aefa903611b7a7faf714
ciec20151104703 <823183518@qq.com>
2019-03-18 17:23:30 +0800

    上传论文

--- 20151104703_liuyan/20151104703_liuyan_FEPFUS/src/com/imnu/dao/UserDao.java
+++ 20151104703_liuyan/20151104703_liuyan_FEPFUS/src/com/imnu/dao/UserDao.java
@@ -7,7 +7,7 @@ import com.imnu.po.User;
 public interface UserDao {
     public User LoginUser(@Param("u_name") String u_name,@Param("u_pwd") String u_pwd);
 
-	public void registerUser(@Param("u_name")String u_name,@Param("u_pwd") String u_pwd,@Param("u_email") String u_email,@Param("u_phone") String u_phone,@Param("u_type") String u_type);
+	public void registerUser(@Param("u_name")String u_name,@Param("u_pwd") String u_pwd,@Param("u_email") String u_email,@Param("u_phone") String u_phone,@Param("u_type") String u_type, @Param("p_img")String p_img, @Param("p_dirpath")String p_dirpath);
 	
 	public User  findUser(@Param("u_name")String u_name);
 
--- 20151104703_liuyan/20151104703_liuyan_FEPFUS/src/com/imnu/service/UserService.java
+++ 20151104703_liuyan/20151104703_liuyan_FEPFUS/src/com/imnu/service/UserService.java
@@ -5,7 +5,7 @@ import com.imnu.po.User;
 public interface UserService {
 	public User LoginUser(String u_name, String u_pwd);
 
-	public void registerUser(String u_name, String u_pwd, String u_email,String u_phone, String u_type);
+	public void registerUser(String u_name, String u_pwd, String u_email,String u_phone, String u_type, String p_img, String p_dirpath);
 	
 	public User findUser(String u_name);
 	
