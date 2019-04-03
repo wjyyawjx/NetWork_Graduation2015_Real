832aea80c0d3a8f0c2b4d51308b11f985d5959ff
ciec20151104714 <944174132@qq.com>
2019-04-03 14:39:04 +0800

    刘昊忻：添加新功能
    给一般用户添加分类查询功能

---
+++ 20151104714_liuhaoxin/20151104714_liuhaoxin_AthleteInformationSystem/src/com/qst/servlet/general/GeneralAthletesQuery.java
@@ -0,0 +1,73 @@
+package com.qst.servlet.general;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.qst.bean.AthletBean;
+import com.qst.dao.GeneralQueryDao;
+
+/**
+ * Servlet implementation class GeneralAthletesQuery
+ */
+@WebServlet("/GeneralAthletesQuery")
+public class GeneralAthletesQuery extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GeneralAthletesQuery() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// 一般用户的分类查询
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		int sid = (int) session.getAttribute("generalsid");
+		String events = request.getParameter("events");
+		GeneralQueryDao rgdao = new GeneralQueryDao();
+		if (events == "所有信息") {
+			System.err.println("aa");
+			ArrayList<AthletBean> abean = new ArrayList<AthletBean>();
+			abean = (ArrayList<AthletBean>) rgdao.athletList(sid);
+			request.setAttribute("mea", abean);
+		} else {
+			ArrayList<AthletBean> abean = new ArrayList<AthletBean>();
+			abean = (ArrayList<AthletBean>) rgdao.athletclassifyList(sid, events);
+			request.setAttribute("mea", abean);
+		}
+		// 返回一届运动会的所有项目信息
+		ArrayList<AthletBean> abe = new ArrayList<AthletBean>();
+		abe = (ArrayList<AthletBean>) rgdao.eventsList(sid);
+		request.setAttribute("abe", abe);
+
+		request.getRequestDispatcher("general-jsp/general-athlet.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- 20151104714_liuhaoxin/20151104714_liuhaoxin_AthleteInformationSystem/src/com/qst/servlet/general/GeneralAthletesView.java
+++ 20151104714_liuhaoxin/20151104714_liuhaoxin_AthleteInformationSystem/src/com/qst/servlet/general/GeneralAthletesView.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import com.qst.bean.AthletBean;
 import com.qst.dao.GeneralQueryDao;
 
@@ -35,6 +37,11 @@ public class GeneralAthletesView extends HttpServlet {
 		response.setContentType("text/html;charset=utf-8");
 		request.setCharacterEncoding("UTF-8");
 		int sid =Integer.parseInt(request.getParameter("sid"));
+		//记录sid
+		HttpSession session = request.getSession();
+		session.setAttribute("generalsid",sid);
+		
+		
 		GeneralQueryDao rgdao = new GeneralQueryDao();
 		
 		//返回一届运动会的所有运动员信息
